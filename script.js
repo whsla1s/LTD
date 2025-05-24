b3032007b775b584fa3fad4f2fbdf6d1ad350c63
matheuscar9 <matheuscar303@gmail.com>
2025-05-23 23:47:26 -0300

    Ultima Alteração

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function mostrarTela(id) {
-    const telas = document.querySelectorAll('.tela');
-    telas.forEach(tela => tela.classList.remove('ativa'));
 
-    const telaSelecionada = document.getElementById(id);
-    if (telaSelecionada) {
-        telaSelecionada.classList.add('ativa');
-    }
-}
